a4a328d51e4aa8fe822ef803a7569ee8a7ce9200
Abey Onalaja <abey.onalaja@gmail.com>
2018-02-23 14:25:46 +0000

    Add models, and update section

--- Main.elm
+++ Main.elm
@@ -3,6 +3,64 @@ module Main exposing (..)
 import Html exposing (..)
 
 
+-- model
+
+
+type alias Model =
+    { players : List Player
+    , name : String
+    , playerId : Maybe Int
+    , plays : List Play
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , points : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerId : Int
+    , name : String
+    , points : Int
+    }
+
+
+initModel : Model
+initModel =
+    { players = []
+    , name = ""
+    , playerId = Nothing
+    , plays = []
+    }
+
+
+
+-- update
+
+
+type Msg
+    = Edit Player
+    | Score Player Int
+    | Input String
+    | Save
+    | Cancel
+    | DeletePlay Play
+
+
+updeate : Msg -> Model -> Model
+updeate msg model =
+    case msg of
+        Input name ->
+            { model | name = name }
+
+        _ ->
+            model
+
+
 main : Html msg
 main =
-    text "Hello ScoreKeeper"
+    text "Hello"
